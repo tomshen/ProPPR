1ec312a7096862f7802b910f90a71a3f5086d2bf
Tom Shen <tom@shen.io>
2015-03-27 02:06:33 -0400

    Process label graphs in parallel

--- src/java/main/edu/cmu/ml/proppr/Propagator.java
+++ src/java/main/edu/cmu/ml/proppr/Propagator.java
@@ -16,6 +16,7 @@ import gnu.trove.map.hash.TIntObjectHashMap;
 import gnu.trove.procedure.TIntDoubleProcedure;
 import gnu.trove.procedure.TIntObjectProcedure;
 import org.apache.log4j.Logger;
+import scala.util.control.TailCalls;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -23,7 +24,7 @@ import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
 
 /**
  * Created by tom on 2/19/15.
@@ -39,37 +40,91 @@ public class Propagator {
                 DEFAULT_LOAD, nthreads));
     }
 
+    private static class SRWRunner implements Callable<SRWResult> {
+        private final JuntoGraph graph;
+        private final SRW srw;
+
+        private SRWRunner(JuntoGraph graph, SRW srw) {
+            this.graph = graph;
+            this.srw = srw;
+        }
+
+        @Override
+        public SRWResult call() throws Exception {
+            TIntDoubleMap startVec = new TIntDoubleHashMap();
+            startVec.put(this.graph.getStartNode(), 1.0);
+
+            ParamVector params = createParamVector(1);
+            this.srw.setupParams(params);
+
+            TIntDoubleMap resultVec = this.srw.rwrUsingFeatures(this.graph.getG(), startVec, params);
+            return new SRWResult(resultVec, this.graph.getLabel());
+        }
+    }
+
+    private static class SRWResult {
+        private TIntDoubleMap resultVec;
+        private String label;
+
+        public SRWResult(TIntDoubleMap resultVec, String label) {
+            this.resultVec = resultVec;
+            this.label = label;
+        }
+
+        public TIntDoubleMap getResultVec() {
+            return resultVec;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+    }
+
+    private static TIntObjectMap<Map<String,Double>> executeSRW(JuntoGraphStreamer graphs, SRW srw, int nthreads) {
+        final TIntObjectMap<Map<String,Double>> nodeLabels = new TIntObjectHashMap<>();
+        ExecutorService pool = Executors.newFixedThreadPool(nthreads);
+
+        Collection<Callable<SRWResult>> tasks = new ArrayList<>();
+
+        for (JuntoGraph graph : graphs) {
+            tasks.add(new SRWRunner(graph, srw));
+        }
+
+        try {
+            List<Future<SRWResult>> results = pool.invokeAll(tasks);
+            for(Future<SRWResult> result : results) {
+                final SRWResult r = result.get();
+                r.getResultVec().forEachEntry(new TIntDoubleProcedure() {
+                    @Override
+                    public boolean execute(int node, double weight) {
+                        nodeLabels.putIfAbsent(node, new HashMap<String, Double>());
+                        nodeLabels.get(node).put(r.getLabel(), weight);
+                        return true;
+                    }
+                });
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return nodeLabels;
+    }
+
     public static void main(String[] args) throws IOException {
         ParsedFile graphFile = new ParsedFile(args[0]);
         final BufferedWriter resultsWriter = Files.newBufferedWriter(FileSystems.getDefault().getPath(args[1]),
                 Charset.defaultCharset());
         int nthreads = Integer.parseInt(args[2]);
+
         SRW srw = new SRW<>(new SRWOptions(iterations));
-
-        long start = System.currentTimeMillis();
-        LearningGraphBuilder graphBuilder = new SimpleLearningGraph.SLGBuilder();
-
-        final TIntObjectMap<Map<String,Double>> nodeLabels = new TIntObjectHashMap<>();
 
         JuntoGraphStreamer graphs = new JuntoGraphStreamer(graphFile, new SimpleLearningGraph.SLGBuilder());
 
-        for (final JuntoGraph graph : graphs) {
-            TIntDoubleMap startVec = new TIntDoubleHashMap();
-            startVec.put(graph.getStartNode(), 1.0);
+        long start = System.currentTimeMillis();
 
-            ParamVector params = createParamVector(nthreads);
-            srw.setupParams(params);
+        final TIntObjectMap<Map<String,Double>> nodeLabels = executeSRW(graphs, srw, nthreads);
 
-            TIntDoubleMap resultVec = srw.rwrUsingFeatures(graph.getG(), startVec, params);
-            resultVec.forEachEntry(new TIntDoubleProcedure() {
-                @Override
-                public boolean execute(int node, double weight) {
-                    nodeLabels.putIfAbsent(node, new HashMap<String, Double>());
-                    nodeLabels.get(node).put(graph.getLabel(), weight);
-                    return true;
-                }
-            });
-        }
         log.info("Finished SRW in " + (System.currentTimeMillis() - start) + " ms");
 
         nodeLabels.forEachEntry(new TIntObjectProcedure<Map<String, Double>>() {
